18ac916d83672e21e0045d8577ea6397ad9a9a6f
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-01-23 17:56:31 -0300

    first commit, exercises 1 to 9

--- .learn/resets/010-swap_digits/app.py
+++ .learn/resets/010-swap_digits/app.py
@@ -1,7 +1,7 @@
 # Complete the function to return the swapped digits of a given two-digit integer
 def swap_digits(num):
   # Your code here
-  return (str(num))
+  return None
    
 # Invoke the function with any two-digit integer as its argument
 print(swap_digits(30))
--- exercises/008-two_timestamps/app.py
+++ exercises/008-two_timestamps/app.py
@@ -1,7 +1,9 @@
 def two_timestamp(hr1, min1, sec1, hr2, min2, sec2):
-    # Your code here   
-    return None
+    # Your code here 
+    hour1= hr1*3600 + min1*60 + sec1
+    hour2= hr2*3600 + min2*60 + sec2
+    return hour2 - hour1
 
 
 # Invoke the function and pass two timestamps(6 intergers) as its arguments
-print(two_timestamp(1,1,1,2,2,2))
+print(two_timestamp(1,2,30,1,3,20))
