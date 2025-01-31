a21f652e4e81a8dd0728e098b1198ae6fce1bd24
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-01-31 17:45:25 -0300

    exercises 38 to 39

--- exercises/038-sort-tuples-ascending/app.py
+++ exercises/038-sort-tuples-ascending/app.py
@@ -1,8 +1,9 @@
 from operator import itemgetter
 
 # Your code here
-def sort_tuples_ascending(name, age, score):
-
+def sort_tuples_ascending(input):
+    result = sorted(input, key=itemgetter(0,1,2))
+    return result
 
 
 print(sort_tuples_ascending(['Tom,19,80', 'John,20,90', 'Jony,17,91', 'Jony,17,93', 'Jason,21,85']))
--- exercises/039-class-that-iterates/app.py
+++ exercises/039-class-that-iterates/app.py
@@ -1,1 +1,16 @@
 # Your code here
+class Iterator:
+    def __init__(self, n):
+        self.n = n
+    
+    def divisible_by_seven(self):
+        for num in range(0, self.n + 1):
+            if num % 7 == 0:
+                yield num
+
+
+obj = Iterator(50)
+
+for num in obj.divisible_by_seven():
+    print (num)
+
